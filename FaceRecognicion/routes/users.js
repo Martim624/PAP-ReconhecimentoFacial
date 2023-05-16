507d4e05a42bdf03e22856061f19744653fcf1b9
Martim624 <72946013+Martim624@users.noreply.github.com>
2023-05-16 12:00:01 +0100

    Register Update v1

--- FaceRecognicion/routes/users.js
+++ FaceRecognicion/routes/users.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcryptjs')
 
 const app = express()
 
 app.use('/assets', express.static('../assets'))
+
+//User model 
+const User = require('../models/User')
+
 
 router.get('/login',(req, res ) => res.render('login'))
 
@@ -23,7 +28,7 @@ router.post('/register', (req, res) => {
     }
 
     if(password !== password2) {
-        erros.push({ msg: 'Passwords do not match '});
+        errors.push({ msg: 'Passwords do not match '});
     }
 
     if(password.length < 6) {
@@ -41,7 +46,40 @@ router.post('/register', (req, res) => {
             password2
         })
     } else {
-        res.send('pass');
+        //Validation passed
+        User.findOne({email: email})
+            .then(user => {
+                if(user) {
+                    //User Existes
+                    errors.push({ msg: 'Email is already registered'})
+                    res.render('register', {
+                        errors,
+                        name,
+                        email,
+                        password,
+                        password2
+                    });
+                } else {
+                    const newUser = new User({
+                        name,
+                        email,
+                        password
+                    });
+
+                    bcrypt.genSalt(10, (err,salt) =>
+                        bcrypt.hash(newUser.password, salt, (err,hash) => {
+                            if(err) throw err;
+                            // Set pasword to hashed
+                            newUser.password = hash; 
+                            // Save user
+                            newUser.save()
+                            .then(user =>{
+                                res.redirect('login');
+                            })
+                            .catch(err => console.log(err));
+                        }))
+                }
+        });
     }
 })
 
